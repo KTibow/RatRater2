a9fd9cc38ed887c8bf89c34bb1e1326fb0d9bc15
Kendell R <KTibow@users.noreply.github.com>
2022-10-09 09:46:40 -0700

    gh pages + format

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
+  kit: {
+    adapter: adapter(),
+  },
 
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	]
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 };
 
 export default config;
