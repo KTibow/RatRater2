4d76cae9caf11f12264c876679f34651f94c3cb7
Kendell R <KTibow@users.noreply.github.com>
2022-11-06 09:02:32 -0800

    fix waves(?)

--- src/lib/Panel/Background.svelte
+++ src/lib/Panel/Background.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-  class="flex-grow bg-[url(waves.svg)] bg-[length:auto_100vmax] p-4 2xl:bg-[length:auto_150vh]"
+  class="flex-grow bg-[url(/waves.svg)] bg-[length:auto_100vmax] p-4 2xl:bg-[length:auto_150vh]"
   class:animate-wave={!activeFile}
 >
   <slot />
