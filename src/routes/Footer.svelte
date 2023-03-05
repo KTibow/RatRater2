335ba273938e4fd8a53679d2cb56935c13930771
Kendell R <KTibow@users.noreply.github.com>
2023-03-05 08:50:21 -0800

    fix vanta link

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,1 +1,1 @@
-<p class="mt-auto">Made by KTibow with <a href="https://discord.gg/RMds4YeWMN">Vanta</a></p>
+<p class="mt-auto">Made by KTibow with <a href="https://discord.gg/7sBMTv7AF3">Vanta</a></p>
