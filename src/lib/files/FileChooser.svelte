00870541c41352cc67612e97f80175004e03e1e7
dxxxxy <introsdream@gmail.com>
2023-03-05 15:19:52 -0500

    fix: only accept .jar files

--- src/lib/files/FileChooser.svelte
+++ src/lib/files/FileChooser.svelte
@@ -1,10 +1,11 @@
 <script>
-  import { Button } from "m3-svelte";
+  import { Button, Snackbar, SnackbarPlacer } from "m3-svelte";
   import { createEventDispatcher } from "svelte";
 
   const dispatch = createEventDispatcher();
   /** @type HTMLInputElement */
   let chooser;
+  let jar
 </script>
 
 <input
@@ -15,7 +16,14 @@
   on:change={() => {
     const file = chooser.files[0];
     if (!file) return;
+    if (!file.name.endsWith(".jar")) {
+      jar = true
+      return
+    }
     dispatch("chosen", file);
   }}
 />
 <Button type="filled" on:click={() => chooser.click()}>Choose a file</Button>
+<SnackbarPlacer>
+  <Snackbar bind:open={jar}>We do not support files other than .jar</Snackbar>
+</SnackbarPlacer>
