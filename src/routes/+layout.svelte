0d22b3144119783175dbd939d0f765ac46a0eea4
KTibow <KTibow@users.noreply.github.com>
2026-02-21 18:32:40 -0800

    add back rat, plus other reorganization

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
+  import ratIcon from "../rat-icon.svg";
+  import rat from "../rat.jpg";
   import "../app.css";
 </script>
 
+<svelte:head>
+  <link rel="icon" href={ratIcon} />
+  <meta property="og:image" content={rat} />
+</svelte:head>
 <slot />
