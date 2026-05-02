c8bcb930e863f76b29277d9309b50532b00558f1
KTibow <KTibow@users.noreply.github.com>
2026-05-02 13:19:44 -0700

    remove rat to peer stuff (was a bad approach + not used)

--- src/routes/FileAltInputs.svelte
+++ src/routes/FileAltInputs.svelte
@@ -1,71 +1,11 @@
 <script lang="ts">
   import { Icon } from "m3-svelte";
   import iconFile from "@ktibow/iconset-ic/outline-file-present";
-  import { onMount } from "svelte";
-  import { sharedAxisTransition } from "m3-svelte";
-  import { page } from "$app/stores";
   import { loadFile } from "$lib/state";
 
   let currentX = 0,
     currentY = 0,
     hide = true;
-
-  let hashStatus: { shown: boolean; text?: string; hideTimeout?: number } = { shown: false };
-  const setHashStatus = (text: string) => {
-    const hideTimeout = setTimeout(() => {
-      hashStatus = { shown: false };
-    }, 5000);
-    if (hashStatus.hideTimeout) clearTimeout(hashStatus.hideTimeout);
-    hashStatus = { shown: true, text, hideTimeout };
-  };
-  const receiveFile = async () => {
-    const hash = $page.url.searchParams.get("rat-to-peer-hash");
-    if (!hash) return;
-    setHashStatus("Connecting");
-
-    const ws = new WebSocket("wss://rat-to-peer.onrender.com");
-    await new Promise((resolve) => ws.addEventListener("open", resolve));
-    ws.send(JSON.stringify({ type: "get-file", hash }));
-
-    setHashStatus("Receiving file");
-    const replyMessage: MessageEvent = await new Promise((resolve) => {
-      const handleMessage = (m: MessageEvent) => {
-        ws.removeEventListener("message", handleMessage);
-        resolve(m);
-      };
-      ws.addEventListener("message", handleMessage);
-    });
-    const reply:
-      | { type: "error"; message: string }
-      | { type: "success"; data: string; name: string } = JSON.parse(replyMessage.data);
-    console.log("received file as", reply);
-    if (reply.type == "error") {
-      setHashStatus(reply.message);
-      if (ws.readyState == WebSocket.OPEN) ws.close();
-      return;
-    }
-    setHashStatus("File received");
-
-    const file = new File([Uint8Array.from(reply.data, (c) => c.charCodeAt(0))], reply.name);
-    loadFile(file);
-  };
-  const downloadFile = async () => {
-    const url = $page.url.searchParams.get("rat-to-peer-url");
-    if (!url) return;
-
-    setHashStatus("Receiving file");
-    const resp = await fetch(
-      "https://rat-to-peer.onrender.com/file?url=" + encodeURIComponent(url),
-    );
-    const data = await resp.blob();
-    console.log("received file as", data);
-    setHashStatus("File received");
-
-    const file = new File([data], url.split("/").at(-1) as string);
-    loadFile(file);
-  };
-
-  onMount(() => Promise.all([receiveFile(), downloadFile()]));
 </script>
 
 <svelte:window
@@ -99,15 +39,6 @@
 >
   <Icon icon={iconFile} width="2rem" height="2rem" />
 </div>
-{#if hashStatus.shown}
-  <div
-    class="hash-status rounded-md bg-primary-container p-4 text-on-primary-container"
-    transition:sharedAxisTransition={{ direction: "Y", rightSeam: false }}
-  >
-    <p class="font-bold">rat-to-peer</p>
-    <p>{hashStatus.text}</p>
-  </div>
-{/if}
 
 <style lang="postcss">
   .dragover-positioning {
@@ -123,10 +54,5 @@
   .custom-hide {
     clip-path: circle(0);
     opacity: 0;
-  }
-  .hash-status {
-    position: fixed;
-    bottom: 1rem;
-    right: 1rem;
   }
 </style>
